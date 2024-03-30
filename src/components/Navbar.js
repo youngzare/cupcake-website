c1c32b3039c6573d9e110eedfdb7693f217a7be7
youngzare <156242246+youngzare@users.noreply.github.com>
2024-03-30 19:44:30 -0400

    completed website

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div className="navbar">
+        <div className='leftNav'></div>
+        <div className='rightNav'></div>
+        <img src={}/>
+    </div>
+  )
+}
+
+export default navbar
