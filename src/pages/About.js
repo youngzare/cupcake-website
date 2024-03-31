47203ec5545854a2555a1106396174223a090a46
youngzare <156242246+youngzare@users.noreply.github.com>
2024-03-31 14:13:42 -0400

    css updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,13 +17,7 @@ function About() {
           fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
           accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
           molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
+          officia aut!
         </p>
       </div>
     </div>
